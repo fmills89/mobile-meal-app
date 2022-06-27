c45a1a263e32c4c437031adf9c05472b36753d88
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-06-27 16:10:32 -0500

    added ionicons addl styling to drawer nav

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
@@ -14,9 +15,36 @@ const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Categories" component={CategoriesScreen} />
-      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#cccccc' },
+        headerTintColor: 'white',
+        sceneContainerStyle: { backgroundColor: '#cccccc' },
+        drawerContentStyle: { backgroundColor: '#cccccc' },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: 'black',
+        drawerActiveBackgroundColor: '#71B671',
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: 'All Cetegories',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
@@ -37,11 +65,17 @@ export default function App() {
             name="Drawer"
             component={DrawerNavigator}
             options={{
-              title: 'Meals Categories',
+              headerShown: false,
             }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-          <Stack.Screen name="MealDetails" component={MealDetailsScreen} />
+          <Stack.Screen
+            name="MealDetails"
+            component={MealDetailsScreen}
+            options={{
+              title: 'About The Meal',
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
