a14d286fd3cefe3b7383f14ef1ac9680a8c74485
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-06-30 13:21:48 -0500

    install redux creating store

--- App.js
+++ App.js
@@ -4,48 +4,54 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Ionicons } from '@expo/vector-icons';
+import { Provider } from 'react-redux';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailsScreen from './screens/MealDetailsScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
+import { store } from './store/redux/store';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: '#cccccc' },
-        headerTintColor: 'white',
-        sceneContainerStyle: { backgroundColor: '#cccccc' },
-        drawerContentStyle: { backgroundColor: '#cccccc' },
-        drawerInactiveTintColor: 'white',
-        drawerActiveTintColor: 'black',
-        drawerActiveBackgroundColor: '#71B671',
-      }}
-    >
-      <Drawer.Screen
-        name="Categories"
-        component={CategoriesScreen}
-        options={{
-          title: 'All Cetegories',
-          drawerIcon: ({ color, size }) => (
-            <Ionicons name="list" color={color} size={size} />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="Favorites"
-        component={FavoritesScreen}
-        options={{
-          drawerIcon: ({ color, size }) => (
-            <Ionicons name="star" color={color} size={size} />
-          ),
-        }}
-      />
-    </Drawer.Navigator>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: '#cccccc' },
+            headerTintColor: 'white',
+            sceneContainerStyle: { backgroundColor: '#cccccc' },
+            drawerContentStyle: { backgroundColor: '#cccccc' },
+            drawerInactiveTintColor: 'white',
+            drawerActiveTintColor: 'black',
+            drawerActiveBackgroundColor: '#71B671',
+          }}
+        >
+          <Drawer.Screen
+            name="Categories"
+            component={CategoriesScreen}
+            options={{
+              title: 'All Cetegories',
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="list" color={color} size={size} />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="Favorites"
+            component={FavoritesScreen}
+            options={{
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="star" color={color} size={size} />
+              ),
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
